9520677ca88f7a891fc84d3cab87c1284ecf213d
Jonathan Jenin <jonathan.jenin@gmail.com>
2022-04-30 13:09:37 -0400

    Fixing seeding missing column and validation conflicts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 require 'faker'
 
 if User.count == 0
-puts "will create trainer..."
-user = User.create!({
-  email: "user@mewbnb.com",
-  password: "password123"
-})
+  puts "will create trainer..."
+  user = User.create!({
+    email: "user@mewbnb.com",
+    password: "password123"
+  })
 end
 
 10.times do
@@ -22,15 +22,14 @@ end
     rate: rand(50..500),
     description: Faker::Games::Pokemon.move,
     location: Faker::Games::Pokemon.location,
-    user: user,
+    user: User.first,
     pokemon_type: ["normal", "fire", "water", "grass", "electric", "ice", "fighting", "poison", "psychic", "ground", "flying", "bug", "rock", "ghost", "dark", "dragon", "steel", "fairy"].sample,
     level: rand(1..100)
   })
   puts "will create booking..."
   Booking.create!({
-    user: user,
     pokemon: pokemon,
-    status: ["available", "confirmed", "rejected"].sample,
+    status: ["available", "requested", "booked"].sample,
     start_date: Faker::Date.between(from: 10.days.ago, to: Date.today),
     end_date: Faker::Date.between(from: Date.today, to: 30.days.from_now)
   })
