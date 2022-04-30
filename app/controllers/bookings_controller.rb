e54bd7c9a29b93376b4d125e535fe3fe709bcd77
Randja Raharijaona <randzoh@outlook.com>
2022-04-30 14:33:37 -0400

    added 'available' as default status for a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,25 @@
 class BookingsController < ApplicationController
   before_action :find_booking, only: [:show, :edit, :update]
-  before_action :find_user
-
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   @booking.user_id = @user.id
-  #   if @booking.save!
-  #     redirect_to booking_path(@booking)
-  #   else
-  #     render :new
-  #   end
-  # end
+  before_action :find_user, :find_pokemon
 
   # def edit; end
 
-  # def new
-  #   @booking = Booking.new
-  # end
-
-  def show
+  def new
+    @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.pokemon = @pokemon
+    if @booking.save!
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+   end
+
+  def show; end
 
   # def update
   #   @booking.update(booking_params)
@@ -33,12 +33,15 @@ class BookingsController < ApplicationController
     @user = current_user
   end
 
+  def find_pokemon
+    @pokemon = current_pokemon
+  end
+
   def find_booking
     @booking = Booking.find(params[:id])
   end
 
   def booking_params
-    params.require(:booking).permit(:name, :type, :level,
-                                   :location, :rate, :description, :picture)
+    params.require(:booking).permit(:status, :start_date, :end_date)
   end
 end
