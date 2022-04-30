00ca2867af67a678ceefe9e2774839d8164db379
Randja Raharijaona <randzoh@outlook.com>
2022-04-30 14:55:57 -0400

    added new and create routes for pokemon and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   # Routes for pokemons
-  resources :pokemons, only: [:index, :show] do
+  resources :pokemons, only: [:index, :show, :new, :create] do
     # Nested route on pokemons for bookings
-    resources :bookings, only: [:show]
+    resources :bookings, only: [:show, :new, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
