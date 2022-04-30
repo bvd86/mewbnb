e56a06d4c25ff11653a4ed291effe2153cfdc51a
Randja Raharijaona <randzoh@outlook.com>
2022-04-30 11:32:27 -0400

    seeding

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,5 +2,5 @@ class Pokemon < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
 
-  validates :name, :rate, :description presence: true
+  validates :name, :rate, :description, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "will create trainer..."
+trainer = User.create!({
+  email: "trainer@mewbnb.com",
+  password: "password123"
+})
+
+puts "will create gym_leader..."
+gym_leader = User.create!({
+  email: "gymleader@mewbnb.com",
+  password: "password123"
+})
+
+
+10.times do
+  puts "will create pokemon..."
+  pokemon = Pokemon.create!({
+    name: Faker::Games::Pokemon.name,
+    rate: rand(50..500),
+    description: Faker::Games::Pokemon.move,
+    location: Faker::Games::Pokemon.location,
+    user: gym_leader
+  })
+  puts "will create booking..."
+  Booking.create!({
+    user: trainer,
+    pokemon: pokemon,
+    status: ["pending", "confirmed", "rejected"].sample,
+    start_date: Faker::Date.between(from: 10.days.ago, to: Date.today),
+    end_date: Faker::Date.between(from: Date.today, to: 30.days.from_now)
+  })
+end
