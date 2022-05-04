97b8c47bae848c36712819645483f79d8a57cb72
Jonathan Jenin <jonathan.jenin@gmail.com>
2022-05-03 21:54:47 -0400

    Allowed to destroy a pokemon.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   # Routes for pokemons
-  resources :pokemons, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :pokemons do
     # Nested route on pokemons for bookings
     resources :bookings, only: [:show, :new, :create]
   end
