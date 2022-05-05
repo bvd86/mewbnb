27ea45d44fd58746f960e1a4e6c0d09c0e69599d
Wendy Pham <wendypham_s2@hotmail.com>
2022-05-04 23:17:28 -0400

    style booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -25,7 +25,7 @@ class BookingsController < ApplicationController
     @booking.pokemon = Pokemon.find(params[:pokemon_id])
     @booking.status = "Booked"
     if @booking.save!
-      redirect_to bookings_path
+      redirect_to booking_path(@booking)
     else
       render :new
     end
