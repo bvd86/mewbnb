0b2512021e4f72e629d21202a005f922ca5b3bb3
Wendy Pham <wendypham_s2@hotmail.com>
2022-05-07 13:27:40 -0400

    testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_07_141819) do
+ActiveRecord::Schema.define(version: 2022_05_07_151443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2022_05_07_141819) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["pokemon_id"], name: "index_bookings_on_pokemon_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "pokemon_names", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "pokemons", force: :cascade do |t|
